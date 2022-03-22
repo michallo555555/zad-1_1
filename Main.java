e2c872efca887534ee2226739772b2d3a34a7a1f
michallo555555 <101186827+michallo555555@users.noreply.github.com>
2022-03-22 11:24:40 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,13 @@
+class Main {
+  public static void main(String[] args) {
+    System.out.println("H");
+    System.out.println("e");
+    System.out.println("l");
+    System.out.println("l");
+    System.out.println("o");
+    System.out.println("w");
+    System.out.println("r");
+    System.out.println("l");
+    System.out.println("d");
+  }
+}
